c704534c1aae034645249e58ab769f0226b4de27
Jimena <j.miguez@airlst.com>
2023-12-09 11:04:45 +0100

    Loading in Course Data

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,6 +1,25 @@
 <template>
   <h2>Lesson</h2>
   <p>This is a lesson</p>
-  <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-  <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+  <p>{{ chapter.title }}</p>
+  <p>{{ lesson.title }}</p>
 </template>
+<script setup>
+const course = useCourse();
+const route = useRoute();
+
+// search for the chapter in the useCourse composable which has a slug matching the
+//chapterSlug route parameter
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug,
+  );
+});
+
+// Go to the current chapter and look for a lesson which has a slug matching the lessonSlug route parameter
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug,
+  );
+});
+</script>
