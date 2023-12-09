e0ab9eb1635f14979a30624b217722303e594148
Jimena <j.miguez@airlst.com>
2023-12-09 11:25:21 +0100

    Add VideoPlayer Component

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- this iframe is the embedded code provided by vimeo -->
+  <!-- makes the src prop dynamic to pass whatever video id we want -->
+  <iframe
+    width="560"
+    height="315"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    title="Video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</template>
+
+<script setup>
+const props = defineProps({
+  videoId: {
+    type: String,
+    required: true,
+  },
+});
+</script>
